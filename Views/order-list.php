40db68d49ada536d78d4efbee615b3e220f4ea24
echeverriaalex <aleex.naahuel@outlook.com>
2023-02-24 19:20:07 -0300

    agrego edicion de los estados de las ordenes y nueva info de los clientes

--- Views/order-list.php
+++ Views/order-list.php
@@ -22,18 +22,53 @@
                          <th>Descripcion</th>
                          <th>Tecnico</th>
                          <th>Cliente</th>
+                         <th>Telefono</th>
                          <th>Opcion</th>
                     </thead>
                     <tbody>
                          <?php 
                          foreach($ordersList as $order){                       
                          ?>
-                              <tr>                             
+                              <tr>
                                    <td> <?php echo $order->getOrderId();?></td>
-                                   <td> <?php echo $order->getOrderStatusId();?></td>
+                                   <td>
+                                        <form action="<?php echo FRONT_ROOT?>Order/EditStatus" method="post">
+                                             <input name="orderId" value="<?php echo $order->getOrderId();?>" hidden>
+                                             <select name="orderStatusId" class="form-control">
+                                                  <optgroup>
+                                                       <option disabled selected><?php echo "Actual: ". $order->getOrderStatusId();?></option>                                        
+                                                       <?php
+                                                            foreach($orderStatusList as $orderStatus){
+                                                       ?>
+                                                                 <option value="<?php echo $orderStatus->getOrderStatusId();?>"> <?php echo "Cambiar a: ".$orderStatus->getDescription();?> </option>
+                                                       <?php        
+                                                            }
+                                                       ?>
+                                                  </optgroup>
+                                             </select>
+                                             <button type="submit" class="btn btn-danger ml-auto d-block"> Actualizar estado</button>
+                                        </form>
+                                   </td>
                                    <td> <?php echo $order->getDescription();?></td>
                                    <td> <?php echo $order->getTechnicalId();?></td>
-                                   <td> <?php echo $order->getClientId();?></td>
+                                   <td> 
+                                        <?php 
+                                             // esto era antes de unir nombre con telefono
+                                             // echo $order->getClientId();
+                                             // con el / corto el string en 2 partes para el nombre uso la parte 1
+                                             $telefono = explode('/',  $order->getClientId());
+                                             //var_dump($telefono);
+                                             echo $telefono[0];
+                                        ?>
+                                   </td>
+                                   <td> 
+                                        <?php 
+                                             // con el / corto el string en 2 partes para el telefono uso la parte 2
+                                             $telefono = explode('/',  $order->getClientId());
+                                             //var_dump($telefono);
+                                             echo $telefono[1];
+                                        ?>
+                                   </td>
                                    <td> 
                                         <form class="bg-light-alpha" action="<?php echo FRONT_ROOT ?>Order/Delete">
                                              <div class="row">
