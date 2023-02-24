292aef6d0a2a0b9433aebda14bcc890788207ec5
echeverriaalex <aleex.naahuel@outlook.com>
2023-02-24 00:52:32 -0300

    agrego atributos nuevos

--- Models/class.order.php
+++ Models/class.order.php
@@ -6,11 +6,15 @@
         private $orderId;
         private $orderStatusId;
         private $description;
+        private $technicalId;
+        private $clientId;
 
-        public function __construct($orderId = null, $orderStatusId = null, $description = null){            
+        public function __construct($orderId = null, $orderStatusId = null, $description = null, $technicalId = null, $clientId = null){
             $this->setOrderId($orderId);
             $this->setOrderStatusId($orderStatusId);
             $this->setDescription($description);
+            $this->setTechnicalId($technicalId);
+            $this->setClientId($clientId);
         }
 
         public function getOrderId(){return $this->orderId;}
@@ -21,5 +25,11 @@
 
         public function getDescription(){return $this->description;}
         public function setDescription($description){$this->description = $description;}
+
+        public function getTechnicalId(){return $this->technicalId;}
+        public function setTechnicalId($technicalId){$this->technicalId = $technicalId;}
+
+        public function getClientId(){return $this->clientId;}
+        public function setClientId($clientId){$this->clientId = $clientId;}
     }
 ?>
